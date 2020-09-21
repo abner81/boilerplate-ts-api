54c7af37b9a431bea438c3ca8e3df9d080174cba
Abner Machado <abner81@live.com>
2020-09-21 16:30:34 -0300

    chore: update organization pasts

--- src/data/usecases/authentication/db-authentication-protocols.ts
+++
@@ -1,6 +0,0 @@
-export * from '../add-account/db-add-account-protocols'
-export * from '../../protocols/db/account/load-account-by-email-repository'
-export * from '../../../domain/usecases/authentication'
-export * from '../../protocols/criptografy/hash-comparer'
-export * from '../../protocols/criptografy/encrypter'
-export * from '../../protocols/db/account/update-access-token-repository'
